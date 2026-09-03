6341de5062818dabfcea669a552fc2f6887bc53f
Atul1127 <lbsd224pandey@gmail.com>
2026-09-04 01:22:14 +0530

    Restore scheduler interface compatibility

--- src/scheduler.h
+++ src/scheduler.h
@@ -1,60 +1,72 @@
 #ifndef SCHEDULER_H
 #define SCHEDULER_H
 
-#include <utility>
 #include <vector>
 #include "process.h"
 #include "matplotlibcpp.h"
 
 namespace plt = matplotlibcpp;
 
-// Base interface shared by all CPU scheduling algorithms.
 class SchedulingAlgorithm {
 public:
-    virtual ~SchedulingAlgorithm() = default;
-
     virtual void schedule(std::vector<Process>& processes) = 0;
     virtual void printGanttChart();
     virtual void plotGanttChart();
     virtual void printMetrics(const std::vector<Process>& processes);
+    virtual ~SchedulingAlgorithm() = default;
 
 protected:
-    // {process_id, duration}; process_id 0 represents CPU idle time.
     std::vector<std::pair<int, int>> gantt_chart;
 };
 
 class FCFS : public SchedulingAlgorithm {
 public:
     void schedule(std::vector<Process>& processes) override;
+    void printGanttChart() override;
+    void plotGanttChart() override;
+    void printMetrics(const std::vector<Process>& processes) override;
 };
 
 class SJF : public SchedulingAlgorithm {
 public:
     void schedule(std::vector<Process>& processes) override;
+    void printGanttChart() override;
+    void plotGanttChart() override;
+    void printMetrics(const std::vector<Process>& processes) override;
 };
 
 class PreemptiveSJF : public SchedulingAlgorithm {
 public:
     void schedule(std::vector<Process>& processes) override;
+    void printGanttChart() override;
+    void plotGanttChart() override;
+    void printMetrics(const std::vector<Process>& processes) override;
 };
 
 class PriorityScheduling : public SchedulingAlgorithm {
 public:
     void schedule(std::vector<Process>& processes) override;
+    void printGanttChart() override;
+    void plotGanttChart() override;
+    void printMetrics(const std::vector<Process>& processes) override;
 };
 
 class PreemptivePriorityScheduling : public SchedulingAlgorithm {
 public:
     void schedule(std::vector<Process>& processes) override;
+    void printGanttChart() override;
+    void plotGanttChart() override;
+    void printMetrics(const std::vector<Process>& processes) override;
 };
 
 class RoundRobin : public SchedulingAlgorithm {
+private:
+    int time_quantum;
 public:
     explicit RoundRobin(int tq) : time_quantum(tq) {}
     void schedule(std::vector<Process>& processes) override;
-
-private:
-    int time_quantum;
+    void printGanttChart() override;
+    void plotGanttChart() override;
 };
 
 #endif // SCHEDULER_H
